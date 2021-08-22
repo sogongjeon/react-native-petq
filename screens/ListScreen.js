785b3dbe6bbc12a95c66333cabbc56a424e5ea82
codejune <kbj9704@gmail.com>
2021-08-23 05:20:09 +0900

    Restful API request function test(getData) implement

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {Text, SafeAreaView, View, Button} from 'react-native';
+import {Text, SafeAreaView, View, Button, FlatList} from 'react-native';
 import tw from 'tailwind-react-native-classnames';
 
 const ListScreen = () => {
   console.log('열람 화면');
   const navigation = useNavigation();
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await fetch('http://59.187.226.217:8081/v1/animal/kind');
+      const json = await response.json();
+      setData(json.data.elements);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <SafeAreaView style={tw`flex-auto justify-center items-center bg-white`}>
+      <FlatList
+        data={data}
+        keyExtractor={({id}, index) => id}
+        renderItem={({item}) => (
+          <Text>
+            {item.kind}, {item.kindDetailName}
+          </Text>
+        )}
+      />
       <Button
         title="세부 페이지 이동"
         titleStyle={{}}
